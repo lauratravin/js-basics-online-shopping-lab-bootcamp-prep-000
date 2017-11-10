3e3bac4325d31bb4fa390b6d1438b75a943e117d
Laura M. Travin <laura.travin@gmail.com>
2017-11-10 04:00:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var cart = [];
 
-function getCart() {
- return cart;
+var itemName;
+
+function getCart(itemName) {
+
+   return cart;
 }
 
 function setCart(c) {
@@ -9,12 +12,39 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+
+      var  itemPrice = Math.floor((Math.random() * 10) + 1);
+
+      var newItem = {};
+//      Object.assign(newItem,{[itemName]:itemPrice});
+      cart.push({[itemName]:itemPrice});
+//      cart.push(newItem);
+      console.log(`${itemName} has been added to your cart.`);
+
+      return cart;
 }
 
 function viewCart() {
-  // write your code here
+      var text = 'In your cart, you have ';
+      var i =0;
+      var keys = Object.keys(cart);
+      if (cart.length == 0){
+               console.log('Your shopping cart is empty.');
+
+      } else if(cart.length == 1) {
+            textFinal = text + Object.keys(cart[i]) +' at $'+ cart[i][Object.keys(cart[0])] +'.';
+            console.log(textFinal);
+      }
+
+
+      // {
+      //         for(var i=0; i<cart.length; i++){
+      //            textFinal = text + cart[i].itemName +' at $'+ cart[i].itemPrice +', ';
+      //         }
+      //         text = text.slice(0,-2)+'.';
+      //         console.log(textFinal);
+      // }
 }
 
 function total() {
