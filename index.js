683fc29638c2df2d2349e440dbae073eee378945
Laura M. Travin <laura.travin@gmail.com>
2017-11-10 06:02:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,11 +65,13 @@ function removeFromCart(itemName) {
             for(var i=0; i<cart.length; i++){
 
                   if (cart[i].hasOwnProperty(itemName)){
+                    
                     cart.splice(i, 1);
-                  }
+                    return cart;
+                  } 
 
               }
-            return cart;
+            return 'That item is not in your cart.';
 
 }
 
